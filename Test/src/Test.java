5e643d3f428c2acfa50832cbd984b6878b014ff5
Test <trial@gmail.com>
2017-06-23 12:17:34 +0530

    added the syso

---
+++ Test/src/Test.java
@@ -0,0 +1,16 @@
+
+import java.awt.List;
+import java.util.ArrayList;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		ArrayList lst=new ArrayList();
+		System.out.println("welcome to capgeminii.commm");
+		System.out.println();
+	}
+
+}
